729688c606892c69199bd3165f67d8476d8f7101
Ada-ikemefuna <adaezeikemefuna@yahoo.com>
2022-03-28 09:38:20 +0100

    handled errors

--- src/App.js
+++ src/App.js
@@ -8,19 +8,31 @@ import InputField from './InputField';
 function App() {
   const [ data, setData] = useState({});
   const [ location, setLocation] = useState('');
-  const [ mood, setMood ] = useState('')
+  const [ mood, setMood ] = useState('');
+  const [ error, setError] = useState(null)
+
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&limit=1&appid=e0f5bcaeb8d52012af25236bfa9a1438`
 
 
   const searchLocation = (event) => {
     if(event.key === 'Enter') {
+
       axios.get(url).then((response) => {
+        if(response.status !== 200){
+            setError(true)
+        }
+        setError(null)
         setData(response.data)
-        console.log(response.data)
+        console.log(response)
+      })
+      .catch(err => {
+        const errorMessage = 'Could not load data from that Location'
+        setMood('')
+        setError(errorMessage)
       })
       setLocation('')
-      setMood('Mood:');
+      setMood('Mood:')
     }
     
   }
@@ -36,6 +48,7 @@ function App() {
       searchLocation={searchLocation}
       setLocation={setLocation}
       />
+      { error && <div className='err'> { error } </div>}
       <Details data={data}
       mood={mood}
       setMood={setMood}/>
